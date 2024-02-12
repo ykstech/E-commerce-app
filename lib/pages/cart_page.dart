606ee2ade652b94f7a66a661c9e0e1cd4d854e82
ykstech <admob5364@gmail.com>
2024-02-13 02:22:01 +0530

    done

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app_flutter/providers/cart_provider.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  double total=0;
+   
+  @override
+  Widget build(BuildContext context) {
+     final cart = context.watch<CartProvider>().cart;
+ // Calculate total price
+    total = cart.fold(0, (previousValue, cartItem) {
+      return previousValue + (cartItem['price'] as double);
+    });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.length,
+              itemBuilder: (context, index) {
+                final cartItem = cart[index];
+            
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+                    radius: 30,
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(
+                              'Delete Product',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            content: const Text(
+                              'Are you sure you want to remove the product from your cart?',
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  'No',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  context
+                                      .read<CartProvider>()
+                                      .removeProduct(cartItem);
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  'Yes',
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    ),
+                  ),
+                  title: Text(
+                    cartItem['title'].toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  subtitle: Text('Size: ${cartItem['size']}'),
+                );
+              },
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+            ),
+            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("\$${total.toStringAsFixed(2)}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    )),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(
+                            Theme.of(context).primaryColor)),
+                    child: Text(
+                      "Buy",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_list.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/global_variables.dart';
+import 'package:shop_app_flutter/widgets/product_card.dart';
+import 'package:shop_app_flutter/pages/product_details_page.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+ late List<String> filters;
+  late String selectedFilter;
+  final TextEditingController searchController=TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize filters with 'All' and unique company names
+    filters = ['All', ...getUniqueCompanyNames(products)];
+    selectedFilter = filters[0];
+   // searchController = TextEditingController();
+  }
+
+  // Helper function to get unique company names from the list of products
+  List<String> getUniqueCompanyNames(List<Map<String, dynamic>> products) {
+    Set<String> uniqueCompanyNames = Set<String>();
+    for (var product in products) {
+      uniqueCompanyNames.add(product['company'] as String);
+    }
+    return uniqueCompanyNames.toList();
+  }
+
+  // Helper function to filter products based on user input and selected company
+  List<Map<String, dynamic>> filterProducts(
+      String searchText, String selectedCompany) {
+    if (searchText.isEmpty && selectedCompany == 'All') {
+      return products;
+    } else if (searchText.isEmpty) {
+      return products
+          .where((product) => product['company'] == selectedCompany)
+          .toList();
+    } else if (selectedCompany == 'All') {
+      return products
+          .where((product) =>
+              product['title'].toString().toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    } else {
+      return products
+          .where((product) =>
+              product['title'].toString().toLowerCase().contains(searchText.toLowerCase()) &&
+              product['company'] == selectedCompany)
+          .toList();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
+
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Shoes\nCollection',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+               Expanded(
+                child: TextField(
+                   controller: searchController,
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              itemCount: filters.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8.0,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = filter;
+                      });
+                    },
+                    child: Chip(
+                      backgroundColor: selectedFilter == filter
+                          ? Theme.of(context).colorScheme.primary
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                      side: const BorderSide(
+                        color: Color.fromRGBO(245, 247, 249, 1),
+                      ),
+                      label: Text(filter),
+                      labelStyle: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                 // Filter products based on selected company
+                final searchText = searchController.text.toLowerCase();
+                final filteredProducts =
+                    filterProducts(searchText, selectedFilter);
+
+                if (constraints.maxWidth > 1080) {
+                  return GridView.builder(
+                     itemCount: filteredProducts.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.75,
+                    ),
+                    itemBuilder: (context, index) {
+                     
+                       final product = filteredProducts[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return ProductDetailsPage(hid:index.toString(),product: product);
+                              },
+                            ),
+                          );
+                        },
+                        child: ProductCard(
+                           id:index.toString() as String,
+                          title: product['title'] as String,
+                          price: product['price'] as double,
+                          image: product['imageUrl'] as String,
+                          backgroundColor: index.isEven
+                              ? const Color.fromRGBO(216, 240, 253, 1)
+                              : const Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return ListView.builder(
+                   itemCount: filteredProducts.length,
+                    itemBuilder: (context, index) {
+                      final product = filteredProducts[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return ProductDetailsPage(hid:index.toString(),product: product);
+                              },
+                            ),
+                          );
+                        },
+                        child: ProductCard(
+                          id:index.toString() as String,
+                          title: product['title'] as String,
+                          price: product['price'] as double,
+                          image: product['imageUrl'] as String,
+                          backgroundColor: index.isEven
+                              ? const Color.fromRGBO(216, 240, 253, 1)
+                              : const Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
